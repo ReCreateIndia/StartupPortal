b9582591c6d7acc52aff422335afe43f78e55583
ReCreateIndia <wewillrecreateindia@gmail.com>
2020-10-16 09:30:22 +0530

    bla

--- startup_portal/views.py
+++ startup_portal/views.py
@@ -1,1 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
 
+def hello(request):
+   return HttpResponse("hello world")
+
+def registerStartup(request):  
+    return render(request, "register.html") 
