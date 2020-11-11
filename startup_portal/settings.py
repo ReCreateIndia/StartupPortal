3f82a7fafe24b29789f88c573c0f33b8f26d1736
ReCreateIndia <wewillrecreateindia@gmail.com>
2020-11-11 11:33:54 +0530

    yo

--- startup_portal/settings.py
+++ startup_portal/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'startup_portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "test")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
