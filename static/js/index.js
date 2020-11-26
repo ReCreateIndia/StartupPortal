230c978d7ffeeefcbef46771fe5195a1d33f4b3a
Yash95Garg <yashagrawal0601@gmail.com>
2020-11-26 21:58:59 +0530

    jai

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,7 @@ var config = {
     };
     
     firebase.initializeApp(config);
-
+	document.getElementById('yash').innerHTML = "File Uploaded Successfully";
 	function upload()
 	{
 		var storage = firebase.storage();
@@ -28,7 +28,8 @@ var config = {
 
 		function(){
         thisref.snapshot.ref.getDownloadURL().then(function(downloadURL){
-            document.getElementById("url").value =  downloadURL;
+			document.getElementById("url").value =  downloadURL;
+			document.getElementById('yash').innerHTML = "File Uploaded Successfully";
 		});
 	});
 }
