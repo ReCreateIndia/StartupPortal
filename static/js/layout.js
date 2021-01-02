bf931e05b30cf4ecd4bfdac80006a4c48e0079a4
piyush-soni <42142059+piyush-soni@users.noreply.github.com>
2021-01-02 11:17:05 +0530

    some changes

--- static/js/layout.js
+++ static/js/layout.js
@@ -7,22 +7,22 @@ function changeFor1()
 }
 function changeFor2()
 {
-	document.getElementById('btn2').className = "btn btn-outline-dark btn-lg";
-	document.getElementById('btn1').className = "btn btn-dark btn-lg";
+	document.getElementById('btn1').className = "btn btn-outline-dark btn-lg";
+	document.getElementById('btn2').className = "btn btn-dark btn-lg";
 	document.getElementById('btn3').className = "btn btn-outline-dark btn-lg";
 	document.getElementById('btn4').className = "btn btn-outline-dark btn-lg";
 }
 function changeFor3()
 {
-	document.getElementById('btn3').className = "btn btn-outline-dark btn-lg";
+	document.getElementById('btn1').className = "btn btn-outline-dark btn-lg";
 	document.getElementById('btn2').className = "btn btn-outline-dark btn-lg";
-	document.getElementById('btn1').className = "btn btn-dark btn-lg";
+	document.getElementById('btn3').className = "btn btn-dark btn-lg";
 	document.getElementById('btn4').className = "btn btn-outline-dark btn-lg";
 }
 function changeFor4()
 {
-	document.getElementById('btn3').className = "btn btn-outline-dark btn-lg";
-	document.getElementById('btn2').className = "btn btn-outline-dark btn-lg";
 	document.getElementById('btn1').className = "btn btn-outline-dark btn-lg";
+	document.getElementById('btn2').className = "btn btn-outline-dark btn-lg";
+	document.getElementById('btn3').className = "btn btn-outline-dark btn-lg";
 	document.getElementById('btn4').className = "btn btn-dark btn-lg";
 }
