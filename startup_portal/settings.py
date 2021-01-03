35840ba61f0d9361b2b2b8698d4587b40b6cbfb3
AnubhavWorld <18ucs066@lnmiit.ac.in>
2021-01-03 14:05:11 +0530

    bla

--- startup_portal/settings.py
+++ startup_portal/settings.py
@@ -122,9 +122,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
